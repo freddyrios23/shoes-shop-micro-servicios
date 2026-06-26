e77cfb38f17652b2a08495807e954defc04fb909
freddyrios23 <fre.rios@duocuc.cl>
2026-06-25 20:31:54 -0400

    Finaliza pruebas y HATEOAS en zapatillas

--- zapatillas/src/main/java/com/Zapatillas/zapatillas/assemblers/SexoModelAssembler.java
+++ zapatillas/src/main/java/com/Zapatillas/zapatillas/assemblers/SexoModelAssembler.java
@@ -2,6 +2,7 @@ package com.Zapatillas.zapatillas.assemblers;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import com.Zapatillas.zapatillas.DTO.SexoDTO;
 import com.Zapatillas.zapatillas.controlerV2.SexoControllerV2;
@@ -9,7 +10,7 @@ import com.Zapatillas.zapatillas.controlerV2.SexoControllerV2;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-
+@Component
 public class SexoModelAssembler implements RepresentationModelAssembler<SexoDTO,EntityModel<SexoDTO>>{
 
     @Override
