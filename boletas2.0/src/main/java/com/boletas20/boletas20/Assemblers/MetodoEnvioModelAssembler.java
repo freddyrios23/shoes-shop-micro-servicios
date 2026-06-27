20495c0a50c01afee030e7b0fa53711de32c9e46
freddyrios23 <fre.rios@duocuc.cl>
2026-06-27 16:21:38 -0400

    Corrige configuración y arranque de microservicio boletas

--- boletas2.0/src/main/java/com/boletas20/boletas20/Assemblers/MetodoEnvioModelAssembler.java
+++ boletas2.0/src/main/java/com/boletas20/boletas20/Assemblers/MetodoEnvioModelAssembler.java
@@ -2,12 +2,14 @@ package com.boletas20.boletas20.Assemblers;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import com.boletas20.boletas20.Controllerv2.MetodoEnvioControllerV2;
 import com.boletas20.boletas20.DTO.MetodoEnvioDTO;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+@Component
 public class MetodoEnvioModelAssembler implements RepresentationModelAssembler<MetodoEnvioDTO, EntityModel<MetodoEnvioDTO>> {
 @Override
     public EntityModel<MetodoEnvioDTO> toModel(MetodoEnvioDTO metodoEnvio) {
--- boletas2.0/src/main/java/com/boletas20/boletas20/Config/WebClientConfig.java
+++ boletas2.0/src/main/java/com/boletas20/boletas20/Config/WebClientConfig.java
@@ -1,30 +1,21 @@
 package com.boletas20.boletas20.Config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebClientConfig {
-    
+
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @SuppressWarnings("null")
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
-            }
-        };
+    public WebClient webClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder.build();
     }
-
 }
